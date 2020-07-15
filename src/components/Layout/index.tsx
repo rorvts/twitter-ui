eec74c4498b5a4d9b00093cddc55465ba9128d68
rorvts <rorvts@gmail.com>
2020-07-15 17:41:16 +0200

    Layout component

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Main from "../Main";
+import { Container, Wrapper } from "./styles";
+
+const Layout: React.FC = () => {
+  return (
+    <Container>
+      <Wrapper>
+        {/* <MenuBar /> */}
+        <Main />
+        {/* <SideBar /> */}
+      </Wrapper>
+    </Container>
+  );
+}
